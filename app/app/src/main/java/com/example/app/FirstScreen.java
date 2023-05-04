4928a1d4693b12c829692cf67fbc72bee4c7542e
Mario Tuset <mtusetg@fpcoruna.afundacion.org>
2023-05-04 20:16:39 +0200

    Borradas actividades de Registro y Login para tener una rama más organizada

--- app/app/src/main/java/com/example/app/FirstScreen.java
+++ app/app/src/main/java/com/example/app/FirstScreen.java
@@ -7,6 +7,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
 import com.example.app.R;
 
 public class FirstScreen extends AppCompatActivity {
@@ -27,8 +29,9 @@ public class FirstScreen extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                Intent intent = new Intent(context, LoginActivity.class); // Inicia la pantalla de inicio de sesión
-                startActivity(intent);
+                //Intent intent = new Intent(context, LoginActivity.class); // Inicia la pantalla de inicio de sesión
+                //startActivity(intent);
+                Toast.makeText(context, "Esto inicia la pantalla de login", Toast.LENGTH_LONG).show();
 
             }
         });
@@ -37,8 +40,9 @@ public class FirstScreen extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                Intent intent = new Intent(context, RegisterActivity.class);
-                startActivity(intent);
+                //Intent intent = new Intent(context, RegisterActivity.class);
+                //startActivity(intent);
+                Toast.makeText(context, "Esto inicia la pantalla de registro", Toast.LENGTH_LONG).show();
 
             }
         });
