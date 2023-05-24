5f38abee0027fed6abadeb460e1ec4701710b501
Mario Tuset <mtusetg@fpcoruna.afundacion.org>
2023-05-24 10:23:20 +0200

    Creado modelo de jugadores, así como el endpoint y la actividad para añadirlos

--- rest/FutAPI/futrest06app/models.py
+++ rest/FutAPI/futrest06app/models.py
@@ -6,3 +6,13 @@ class User(models.Model):
     password = models.CharField(max_length=70)
     nickname = models.CharField(max_length=30)
     token = models.CharField(blank=True, null= True, max_length=200)
+
+
+class Player(models.Model):
+    name = models.CharField(max_length=80)
+    age = models.IntegerField()
+    number = models.IntegerField()
+    image = models.CharField(blank=True,null=True, max_length=200)
+    team = models.CharField(max_length=50, default='Sin equipo')
+    nationality = models.CharField(max_length=30)
+
