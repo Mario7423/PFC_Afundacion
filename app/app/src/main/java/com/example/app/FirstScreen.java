95e814bf5bb1955f7e84da1ff703477a324a40d7
Mario7423 <mtusetg@fpcoruna.afundacion.org>
2023-06-01 18:45:33 +0200

    Añadida función para que la sesión se mantenga abierta si el usuario no cierra sesión

--- app/app/src/main/java/com/example/app/FirstScreen.java
+++ app/app/src/main/java/com/example/app/FirstScreen.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -24,6 +25,16 @@ public class FirstScreen extends AppCompatActivity {
 
         login = findViewById(R.id.login); // Instanciamos los botones para poder trabajar sobre ellos
         register = findViewById(R.id.register);
+
+        SharedPreferences preferences = context.getSharedPreferences("SESSIONS_APP_PREFS", MODE_PRIVATE);
+        String token = preferences.getString("VALID_TOKEN", "");
+
+        if(!token.isEmpty()){
+
+            Intent intent = new Intent(context, MainScreen.class);
+            startActivity(intent);
+
+        }
 
         login.setOnClickListener(new View.OnClickListener() {  // onClickListener para poder viajar entre pantallas
             @Override
